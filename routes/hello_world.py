df888b7bb88aee191b655bf2f227be462ed1a042
McWoonWorld <87518277+MCcubeAge@users.noreply.github.com>
2024-03-07 08:08:57 +0200

    Update hello_world.py

--- routes/hello_world.py
+++ routes/hello_world.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 hello_world_bp = Blueprint('hello_world', __name__)
 
-@hello_world_bp.route('/hello_world')
+@hello_world_bp.route('/')
 def hello_world():
     return 'Hello, World!'
